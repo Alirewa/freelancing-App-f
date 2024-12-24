ef7f8a93527b19d678871758bf5412ec0a604d76
Alireza Pourgholam <Alireza.pourgholam444@gmail.com>
2024-12-24 13:02:43 +0330

    Update ProjectHeader.jsx

--- src/Features/singleProject/ProjectHeader.jsx
+++ src/Features/singleProject/ProjectHeader.jsx
@@ -1,5 +1,14 @@
-function ProjectHeader() {
-  return <div>ProjectHeader</div>;
+import { HiArrowRight } from "react-icons/hi";
+
+function ProjectHeader({ project }) {
+  return (
+    <div className="flex items-center gap-x-4 mb-8">
+      <button>
+        <HiArrowRight />
+      </button>
+      <h1>لیست درخواست‌های {project.title}</h1>
+    </div>
+  );
 }
 
 export default ProjectHeader;
